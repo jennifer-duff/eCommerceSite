e02df56ecd83bbd03f664c28aeb07ec0ea5f9966
Jennifer Duff <jduffbowser@gmail.com>
2021-12-30 15:40:18 -0800

    split sylesheets according to partials

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+// Express set-up
 const express = require("express");
 const app = express();
 const path = require('path');
 
+// static assets
 app.use(express.static('public'));
+app.use(express.static('Assets'));
 
+// set up EJS + Views folder & path
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
-
+// set up port listening
 app.listen(8080, () => {
     console.log("Listening on port 8080!")
 })
